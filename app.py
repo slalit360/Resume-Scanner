082fc84cd5be1effbbc496731a24710f8a7b7090
Suthar, Lalit H <lalit.h.suthar@accenture.com>
2021-07-31 15:04:57 +0530

    session fix added

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
-# app.secret_key = os.urandom(12)  # Generic key for dev purposes only
+app.secret_key = os.urandom(24)  # Generic key for dev purposes only
 logger = app.logger
+
 stripe_keys = {
   'secret_key': os.environ['STRIPE_SECRET_KEY'],
   'publishable_key': os.environ['STRIPE_PULISHABLE_KEY']
